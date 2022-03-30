1a73015514ae0a94864544d7f780acfa0278ac0e
LAUDELINO SIMAO CAMPOS ALMEIDA FILHO <laudelino.simao@portoseguro.com.br>
2022-03-30 18:12:13 -0300

    6 - Lec05MonoFromSupplier.java

--- src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
+++ src/main/java/com/rp/sec01/Lec05MonoFromSupplier.java
@@ -3,16 +3,29 @@ package com.rp.sec01;
 import com.rp.courseutil.Util;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 public class Lec05MonoFromSupplier {
 
     public static void main(String[] args) {
 
         // só usamos mono.just quando tevemos data para processar
         //Mono<String> mono = Mono.just(getName());
-        Mono<String> monoSupplier = Mono.fromSupplier(() -> getName());
+
+        //Usando Mono.fromSupplier (fornecedor) mono executa à partir de uma outra fonte
+        Supplier<String> stringSupplier = () -> getName();
+        Mono<String> monoSupplier = Mono.fromSupplier(stringSupplier);
         monoSupplier.subscribe(
                 Util.onNext()
         );
+
+        //Usando Mono.Callable  mono executa diretamente
+        Callable<String> stringCallable = () -> getName();
+        Mono.fromCallable(stringCallable).subscribe(
+                Util.onNext()
+        );
+
     }
 
     private static String getName() {

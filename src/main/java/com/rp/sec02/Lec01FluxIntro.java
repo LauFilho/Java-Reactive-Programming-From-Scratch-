97790329755895c88bf1130a3d5db83162694bce
LAUDELINO SIMAO CAMPOS ALMEIDA FILHO <laudelino.simao@portoseguro.com.br>
2022-04-04 09:40:03 -0300

    16 - Lec06Subscription

--- src/main/java/com/rp/sec02/Lec01FluxIntro.java
+++ src/main/java/com/rp/sec02/Lec01FluxIntro.java
@@ -2,11 +2,7 @@ package com.rp.sec02;
 
 import com.github.javafaker.Faker;
 import com.rp.courseutil.Util;
-import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.lang.reflect.Array;
 
 public class Lec01FluxIntro {
 
